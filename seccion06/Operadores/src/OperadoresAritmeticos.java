44981e883230027549bc7fcea35a9dcde8979002
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-04-06 12:44:10 +0200

    Sección 06: Operadores Aritméticos

---
+++ seccion06/Operadores/src/OperadoresAritmeticos.java
@@ -0,0 +1,25 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("*** Operadores Aritméticos ***");
+        double a = 5, b = 3, resultado;
+        // Suma +
+        resultado = a + b;
+        System.out.println("resultado = " + resultado);
+
+        // Resta -
+        resultado = a - b;
+        System.out.println("resultado = " + resultado);
+
+        // Multiplicación
+        resultado = a * b;
+        System.out.println("resultado = " + resultado);
+
+        // División
+        resultado = a / b;
+        System.out.println("resultado = " + resultado);
+
+        // Múdulo (residuo de la operación)
+        resultado = a % b;
+        System.out.println("resultado = " + resultado);
+    }
+}
