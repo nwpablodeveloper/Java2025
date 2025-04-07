1a26227a8f189c6f79489bed9657ac468b247b91
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-04-07 20:35:39 +0200

    Sección 07: Sentencia IF

--- seccion07/SentenciasDecision/src/SentenciaIf.java
+++ seccion07/SentenciasDecision/src/SentenciaIf.java
@@ -1,5 +1,10 @@
 public class SentenciaIf {
     public static void main(String[] args) {
         System.out.println("*** Sentencia If ***");
+
+        var edad = 30;
+        if (edad >= 18){
+            System.out.println("Eres mayor de edad");
+        }
     }
 }
