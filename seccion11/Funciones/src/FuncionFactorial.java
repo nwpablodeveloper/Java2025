8d2988675480af3dd9b8c524705a8595b5791ad6
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-04-29 16:30:41 +0200

    Sección 11: Función Recursiva

---
+++ seccion11/Funciones/src/FuncionFactorial.java
@@ -0,0 +1,9 @@
+public class FuncionFactorial {
+    //  Calcular el factorial de un número entero positivo usando recursividad.
+    // Ejemplo: factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+    
+
+    public static void main(String[] args) {
+
+    }
+}
