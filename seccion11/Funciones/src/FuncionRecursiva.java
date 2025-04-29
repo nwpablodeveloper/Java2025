b43bf645974cad520f1785096eb2e80fc3ea8be8
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-04-29 16:32:10 +0200

    Sección 11: Función Recursiva

--- seccion11/Funciones/src/FuncionRecursiva.java
+++ seccion11/Funciones/src/FuncionRecursiva.java
@@ -1,5 +1,18 @@
 public class FuncionRecursiva {
+    // Imprimir 1 al 5 usando una función recursiva
+
+    static void funcionRecursiva(int numero){
+        if (numero == 1)
+            System.out.println(numero + " ");
+        else {
+            // Caso recursivo
+            funcionRecursiva(numero-1);
+            System.out.println(numero + " ");
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+       funcionRecursiva(5);
     }
 }
