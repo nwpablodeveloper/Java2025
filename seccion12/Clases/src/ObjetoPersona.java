5de5d03f5a88382a811322b261b4ba19d0d6ec47
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-04-30 16:25:57 +0200

    Sección 12: Creación de un objeto de tipo ObjetoPersona

--- seccion12/Clases/src/ObjetoPersona.java
+++ seccion12/Clases/src/ObjetoPersona.java
@@ -1,2 +1,23 @@
 public class ObjetoPersona {
+    String nombre;
+    String apellido;
+
+    void mostrarPersona(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("apellido: " + apellido);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("*** Creació de Clase y Objeto de tipo objeto1 ***");
+
+        var objeto1 = new ObjetoPersona();
+        objeto1.nombre = "Layla";
+        objeto1.apellido = "Acosta";
+        objeto1.mostrarPersona();
+
+        var objeto2 = new ObjetoPersona();
+        objeto2.nombre = "Ian";
+        objeto2.apellido = "Gomez";
+        objeto2.mostrarPersona();
+    }
 }
