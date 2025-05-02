fb86700e82810763bb53679e063d396da90d0784
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-02 21:52:15 +0200

    Sección 12: Sistema de ventas - Clase Orden Parte 1

---
+++ seccion12/Clases/src/sistema_ventas/Orden.java
@@ -0,0 +1,22 @@
+package sistema_ventas;
+
+public class Orden {
+    private final int idOrden;
+    private Producto[] productos;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+    private static int contadorOrden;
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrden;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto){
+        if (this.contadorProductos < Orden.MAX_PRODUCTOS)
+            this.productos[this.contadorProductos++] = producto;
+        else
+            System.out.println("Se ha superado el máximo de productos");
+    }
+
+}
