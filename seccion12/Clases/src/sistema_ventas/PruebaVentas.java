824044d0715fbe33bb479b6c179ae5d5c4899c7a
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-02 23:24:57 +0200

    Sección 12: PruebaVentas

--- seccion12/Clases/src/sistema_ventas/PruebaVentas.java
+++ seccion12/Clases/src/sistema_ventas/PruebaVentas.java
@@ -5,10 +5,25 @@ public class PruebaVentas {
         System.out.println("*** Sistema de Ventas ***");
 
         var producto1 = new Producto("Blusa", 30);
-        System.out.println(producto1);
-
         var producto2 = new Producto("Zapatos", 50);
-        System.out.println(producto2);
+        var producto3 = new Producto("Campera", 120);
+        var producto4 = new Producto("Medias", 8.75);
+        var producto5 = new Producto("Remera", 33.50);
+        var producto6 = new Producto("Buzo", 48.66);
+        var producto7 = new Producto("Gorra", 25.74);
+        var producto8 = new Producto("Cadena", 5.32);
+        var producto9 = new Producto("Guantes", 11.20);
+        var producto10 = new Producto("Saco", 230.61);
 
+        var orden1 = new Orden();
+        orden1.agregarProducto(producto1);
+        orden1.agregarProducto(producto2);
+        orden1.agregarProducto(producto3);
+        orden1.agregarProducto(producto4);
+        orden1.agregarProducto(producto7);
+        orden1.agregarProducto(producto7);
+        orden1.agregarProducto(producto8);
+
+        orden1.mostrarOrden();
     }
 }
