f7ebeb849f5ec5ba57e813067eac17c7f3d41961
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-05 22:59:09 +0200

    Sección 13: Métdodo comprarSnack creado Maquina de Sanck

--- seccion13/TemasAvanzados/src/maquina_snacks/MaquinaSnacks.java
+++ seccion13/TemasAvanzados/src/maquina_snacks/MaquinaSnacks.java
@@ -5,23 +5,26 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MaquinaSnacks {
+
     public static void main(String[] args) {
         maquinaSnacks();
     }
 
     public static void maquinaSnacks(){
-        var salir = false;
-        var consola = new Scanner(System.in);
+        boolean salir = false;
+        Scanner consola = new Scanner(System.in);
 
         // Creamos la lsita de productos de tipo Snack
         List<Snack> productos = new ArrayList<>();
 
         System.out.println("*** Maquina de Snacks ***");
-        Snacks.mostrarSnacks(); // Mostrar el inventario de Snacks disponibles
+
+        // Mostrar el inventario de Snacks disponibles
+        Snacks.mostrarSnacks();
 
         while (!salir){
             try{
-                var opcion = mostrarMenu(consola);
+                int opcion = mostrarMenu(consola);
                 salir = ejecutarOpciones(opcion, consola, productos);
             }catch (Exception e){
                 System.out.println("Ocurrio un error: " + e);
@@ -36,14 +39,50 @@ public class MaquinaSnacks {
             Menú:
             1. comparar Snack
             2. Mostrar Ticket
-            3. Agregar Nuevo Sncak
+            3. Agregar Nuevo Snack
             4. Salir
             Elige una opción:\s""");
         return Integer.parseInt(consola.nextLine());
     }
 
     public static boolean ejecutarOpciones(int opcion, Scanner consola, List<Snack> productos){
-        return false;
+        var salir = false;
+
+        switch (opcion){
+            case 1 -> comprarSnack(consola, productos);
+            case 2 -> System.out.println("Mostrar Ticket");
+            case 3 -> System.out.println("Agrgar Nuevo Snack");
+            case 4 -> {
+                System.out.println("Saliendo...");
+                salir = true;
+            }
+            default -> {
+                System.out.println("Opcion no valida");
+            }
+        }
+        return salir;
+    }
+
+    public static void comprarSnack(Scanner consola, List<Snack> productos){
+
+        System.out.print("Que snack quiere comprar (id)? ");
+        var idSnack = Integer.parseInt(consola.nextLine());
+
+        // Validar que el snack exista en la lista de snacks
+        var snackEncontrado = false;
+
+        for (Snack snack : Snacks.getSnacks()){
+            if (idSnack == snack.getIdSnack()){
+                // Agregamos el Sncka a la lista de productos
+                productos.add(snack);
+                System.out.println("\tOk, Snack Agregado:\n\t" + snack);
+                snackEncontrado = true;
+                break;
+            }
+        }
+        if (!snackEncontrado){
+            System.out.println("Id de Snack no valido: " + idSnack);
+        }
     }
 }
 
