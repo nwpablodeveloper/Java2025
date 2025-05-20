3313daad32a62a862c6806e02c984ca3a05993bc
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-20 15:50:32 +0200

    No Lombok anda mal - Gets, Sets y toString de forma manual

--- seccion23/recursos-humanos-spring/src/main/java/gm/rh/modelo/Empleado.java
+++ seccion23/recursos-humanos-spring/src/main/java/gm/rh/modelo/Empleado.java
@@ -9,12 +9,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Data
 @Entity
-//@Data
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-//@ToString
 public class Empleado {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idEmpleado;
@@ -22,45 +23,4 @@ public class Empleado {
     private String departamento;
     private double sueldo;
 
-    @Override
-    public String toString() {
-        return "Empleado{" +
-                "idEmpleado=" + idEmpleado +
-                ", nombre='" + nombre + '\'' +
-                ", departamento='" + departamento + '\'' +
-                ", sueldo=" + sueldo +
-                '}';
-    }
-
-    public Integer getIdEmpleado() {
-        return idEmpleado;
-    }
-
-    public void setIdEmpleado(Integer idEmpleado) {
-        this.idEmpleado = idEmpleado;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDepartamento() {
-        return departamento;
-    }
-
-    public void setDepartamento(String departamento) {
-        this.departamento = departamento;
-    }
-
-    public double getSueldo() {
-        return sueldo;
-    }
-
-    public void setSueldo(double sueldo) {
-        this.sueldo = sueldo;
-    }
 }
