4352c75adc3b9c16fea62f5f8f2ea94a8f89dd54
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-21 18:46:34 +0200

    Cagada con Git, no reconocia la carpeta del FrontEnd

---
+++ seccion23/drecursos-humanos-app/src/empleados/ListadoEmpleados.js
@@ -0,0 +1,67 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
+
+export default function ListadoEmpleados() {
+
+    const urlBase = "http://localhost:2204/rh-app/empleados";
+
+    const[empleados, setEmpleados] = useState([]);
+
+    // Se ejecuta al cargar la página
+    useEffect(() => {
+        cargarEmpleados();
+    },[]); // [] indicar vacio para que se ejecute solo 1 vez
+
+    const cargarEmpleados = async () => {
+        const resultado = await axios.get(urlBase);
+        console.log("Resultado de cargar empleados");
+        console.log(resultado.data)
+        setEmpleados(resultado.data)
+    }
+
+  return (
+    <div className='container'>
+        <div className='container text-center' style={{margin: "30px"}}>
+            <h3>Sistema de recursos humanos</h3>
+        </div>
+        <table className="table table-striped table-hover align-middle">
+            <thead className='table-dark'>
+                <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Empleado</th>
+                <th scope="col">Departamento</th>
+                <th scope="col">Sueldo</th>
+                <th>Acciones</th>
+                </tr>
+            </thead>
+            {
+                empleados.map((empleado, indice) => (
+                <tbody>
+                    <tr key={indice}>
+                    <th scope="row">{empleado.idEmpleado}</th>
+                        <td>{empleado.nombre}</td>
+                        <td>{empleado.departamento}</td>
+                        <td>
+                            <NumericFormat value={empleado.sueldo} displayType={'text'}
+                                            thousandSeparator=',' prefix='$ '
+                                            decimalScale={2} fixedDecimalScale>
+
+                            </NumericFormat>
+                        </td> 
+                        <td className='text-center'>
+                            <div>
+                                <Link to={`/editar/${empleado.idEmpleado}`}
+                                        className='btn btn-warning me-3'>Editar</Link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+                ))
+            }
+            </table>
+    </div>
+
+  )
+}
