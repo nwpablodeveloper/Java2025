c466925e145432cf1fcbb811529861e4f90d3211
Pablo Veiga <nwpablodeveloper@gmail.com>
2025-05-22 15:47:01 +0200

    Sección 23: Eliminar Empleado con Spring

--- seccion23/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ seccion23/recursos-humanos-spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("rh-app") //dominio/rh-app
@@ -56,6 +58,19 @@ public class EmpleadoControlador {
             return ResponseEntity.ok(empleadoServicio.guardarEmpleado(empleado));
         }
 
+    }
+
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null)
+            throw new RecursoNoEncontradoExcepcion("No se encotro el empleado por id: " + id);
+
+        empleadoServicio.eliminarEmpleado(empleado);
+//        Json{"eliminado": "true"}
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 
 }
